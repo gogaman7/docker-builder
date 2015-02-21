602966588433e19514f651aef944cf6fa0a2f043
Igor Belayev <igor.belayev@juntacorp.com>
2015-02-20 21:54:30 -0500

    - disabled pushing of image, only building it.

--- build.sh
+++ build.sh
@@ -72,9 +72,9 @@ echo "=> Building and pushing image"
 if [ ! -z "$IMAGE_NAME" ]; then
 	docker build --rm --force-rm -t $IMAGE_NAME .
 
-	echo "   Pushing image $IMAGE_NAME"
-	docker push $IMAGE_NAME
-	docker rmi -f $(docker images -q --no-trunc -a) > /dev/null 2>&1
+	# echo "   Pushing image $IMAGE_NAME"
+	# docker push $IMAGE_NAME
+	# docker rmi -f $(docker images -q --no-trunc -a) > /dev/null 2>&1
 else
 	echo "   WARNING: no \$IMAGE_NAME found - skipping build and push"
 fi
